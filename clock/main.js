33e6e8b9bd5c087fd5b6f5214181378be542c115
Diane Chiang <dianechiang@Dianes-iMac.local>
2019-03-29 20:16:53 -0700

    Updated assignment 2c - vinyl

--- clock/main.js
+++ clock/main.js
@@ -1,5 +1,3 @@
-let angle = 0;
-
 function setup() {
 
   	const canvas = createCanvas(700, 400);
@@ -8,69 +6,76 @@ function setup() {
 }
 
 function draw() {
-  background('#F4F4F4');
+  background(180,100,100);
+
+  background(180,100,100);
   noStroke();
 
+  //Record player
+  fill(180,180,100);
+  rect(60, 60, width/1.18, height/1.4);
 
-  // vinyl
+  // small vinyl 24-hours
   fill(0);
-  ellipse(width/5, height/2, 200);
+  ellipse(width/5, height/2, 100);
 
   fill(255);
-  ellipse(width/5, height/2, 60);
+  ellipse(width/5, height/2, 30);
 
+  // medium vinyl 60-minutes
   fill(0);
-  ellipse(width/2, height/2, 150);
+  ellipse(width/2.3, height/2, 150);
 
   fill(255);
-  ellipse(width/2, height/2, 50);
+  ellipse(width/2.3, height/2, 45);
 
-
+  // large vinyl 60-seconds
   fill(0);
-  ellipse(width/1.35, height/2, 100);
+  ellipse(width/1.35, height/2, 200);
 
   fill(255);
-  ellipse(width/1.35, height/2, 40);
-
-
+  ellipse(width/1.35, height/2, 60);
 
   // map time to circle
   let s = map(second(), 0, 60, 0, radians(360));
   let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, radians(360));
-  let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, radians(360) * 2);
+  let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, radians(360));
 
+  // 24-hour rotating dot
+  fill(180,180,180);
+  ellipse(width/5 + cos(h - radians(90)) * 30, height/2 + sin(h - radians(90)) * 30, 15);
 
+  // 60-minute rotating dot
+  fill(180,180,180);
+  ellipse(width/2.3 + cos(m - radians(90)) * 50, height/2 + sin(m - radians(90)) * 50, 15);
 
-  // hour hand
+  // 60-second rotating dot
+  fill(180,180,180);
+  ellipse(width/1.35 + cos(s - radians(90)) * 70, height/2 + sin(s - radians(90)) * 70, 15);
+
+  // Vinyl record pin - small
   push( );
-  translate(width/2, height/2);
-
-  rotate(h);
-
-  fill(255, 0, 0);
-  rect(-3, -60, 6, 60);
+  translate(width/5, height/2);
+  rotate(radians(120));
+  fill(255,0,0);
+  rect(-4,-60,8,60);
   pop( );
 
-  // minute hand
+  // Vinyl record pin - medium
   push( );
-  translate(width/2, height/2);
-
-  rotate(m);
-
-  fill(0);
-  rect(-2, -90, 4, 90);
+  translate(width/2.3, height/2);
+  rotate(radians(120));
+  fill(255,0,0);
+  rect(-4,-90,8,90);
   pop( );
 
-  // second hand
+  // Vinyl record pin - large
   push( );
-  translate(width/2, height/2);
-
-  rotate(s);
-
-  fill(180, 180, 180);
-  rect(-1, -120, 2, 120);
+  translate(width/1.35, height/2);
+  rotate(radians(120));
+  fill(255,0,0);
+  rect(-4,-115,8,115);
   pop( );
-
 
 
 }
